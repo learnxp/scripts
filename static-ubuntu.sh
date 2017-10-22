dd6abb7f365920d1b3c8526de21ef741e87fdc3b
Charles Severance <csev@umich.edu>
2017-10-22 12:22:10 -0400

    Update static-ubuntu.sh

--- static-ubuntu.sh
+++ static-ubuntu.sh
@@ -26,6 +26,9 @@ sudo apt-get install -y mysql-server
 sudo a2enmod expires
 sudo a2enmod headers
 sodo phpenmod mysqlnd pdo_mysql
+# vi /etc/apache2/apache2.conf
+# AllowOverride All
+
 sudo systemctl restart apache2
 
 # apache2ctl configtest
