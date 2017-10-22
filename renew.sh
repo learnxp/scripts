0650e1a8eb58151ae4de0e92edbf9674ab14fb21
Charles Severance <csev@umich.edu>
2017-10-22 01:05:53 -0400

    Update renew.sh

--- renew.sh
+++ renew.sh
@@ -5,4 +5,5 @@ WOY=$(date +%W)
 sudo rm /tmp/renew-$WOY
 sudo > /tmp/renew-$WOY 2>&1 /usr/bin/certbot renew --post-hook "service apache2 reload"
 
+# 43 6 * * 1 /bin/bash /home/ubuntu/renew.sh
 # mail xx@yy.com < /tmp/renew-$WOY
